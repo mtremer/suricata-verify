8c417619f49db799c2f11ed769b1e60948916e82
Stefan Schantl <stefan.schantl@ipfire.org>
2021-12-01 05:42:51 +0100

    location: Enhance test for new keywords
    This commite adds tests for the following newly introduced rule keywords
    when building suricata with libloc support:
    
    * anycast
    * anyonymous-proxy
    * satellite-provider
    
    Signed-off-by: Stefan Schantl <stefan.schantl@ipfire.org>

--- tests/location/create-database.py
+++ tests/location/create-database.py
@@ -17,6 +17,7 @@ test_data = {
             'ccontinent': 'EU',
             'asnumber': 8560,
             'asname': '1&1 IONOS SE',
+            'flags': ["NETWORK_FLAG_ANYCAST", "NETWORK_FLAG_ANONYMOUS_PROXY", "NETWORK_FLAG_SATELLITE_PROVIDER"],
         }
     }
 
@@ -46,6 +47,13 @@ for addr, data in test_data.items():
     net = db.add_network(addr)
     net.country_code = data["ccode"]
     net.asn = asn.number
+
+    # Check if one ore more network flags should be added to this network.
+    if "flags" in data.keys():
+        # Loop through the list of flags.
+        for flag in data["flags"]:
+            # Add flag to the network.
+            net.set_flag(getattr(location, flag))
 
 # Write the database to disk
 db.write("test.db")
